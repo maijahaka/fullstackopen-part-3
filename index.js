408053f51152761cb24a1652e7cc32e33ee9a2be
Maija Haka <maija.haka@live.fi>
2020-12-31 15:59:09 +0200

    3.11 puhelinluettelo full stack

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('build'))
+
 var morgan = require('morgan')
 morgan.token('body', function (req,res) { return JSON.stringify(req.body) })
 
