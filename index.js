8a60423e71fddb1e1ecbec5570a725135e2d68ba
Maija Haka <maija.haka@live.fi>
2021-01-02 23:19:59 +0200

    3.18: puhelinluettelo ja tietokanta, step6

--- index.js
+++ index.js
@@ -22,29 +22,6 @@ app.use(morgan(
         skip: function (req, res) { return req.method === 'POST' }
 }))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(persons => {
         res.json(persons)
@@ -119,11 +96,16 @@ app.delete('/api/persons/:id', (req, res, next) => {
         .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
     const date = new Date()
 
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>`
-        + `<p>${date}</p>`)
+    Person.count()
+        .then(result => {
+            res.send(`<p>Phonebook has info for ${result} people</p>`
+                + `<p>${date}</p>`)
+        })
+        .catch(error => next(error))
+
 })
 
 const unknownEndpoint = (req, res) => {
