269f24df07a32fa4fb73bbe2cb422496124af468
Maija Haka <maija.haka@live.fi>
2021-01-04 17:02:09 +0200

    3.20: puhelinluettelo ja tietokanta, step8

--- index.js
+++ index.js
@@ -113,8 +113,8 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name == 'CastError') {
         return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError' ) {
-        return res.status(400).send({ error: 'name must be unique' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).send({ error: error.message })
     }
 
     next(error)
